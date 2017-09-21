a3582a61bc82d3a4af768520627951544b805686
Elijah <advadr@gmail.com>
2017-09-21 02:58:45 +0000

    Add Bootstrap

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 @section('title','主页')
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="https://www.xqapp.net">本地社区</a>大法好
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">马上注册</a>
+    </p>
+  </div>
 @stop
