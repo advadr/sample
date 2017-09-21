7d3a69bd820223e00606eb8a73088459e14f31d4
Elijah <advadr@gmail.com>
2017-09-21 05:52:38 +0000

    Add Signup

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogan">
+      <img class="brand-icon" src="http://ohx17pmn4.bkt.clouddn.com/pc-localCommunity-logo.png" alt="logo">
+      <a href="https://www.xqapp.net">
+        本地社区 | 家里事儿，找社区。
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{ route('about') }}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,7 +2,7 @@
 @section('title','主页')
 @section('content')
   <div class="jumbotron">
-    <h1>Hello Laravel</h1>
+    <h1>Hello BendiShequ</h1>
     <p class="lead">
       你现在所看到的是 <a href="https://www.xqapp.net">本地社区</a>大法好
     </p>
@@ -10,7 +10,7 @@
       一切，将从这里开始。
     </p>
     <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">马上注册</a>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">马上注册</a>
     </p>
   </div>
 @stop
